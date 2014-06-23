385b5d91fab0efa94e221a41bacf34bd98a6098f
Mike Liddle <mike.liddle@connorgp.com>
2014-06-23 09:38:31 -0600

    Why won't this work?
    try again.  Automation.

--- CopyrightAddingSoftware/Once edited Repository/LicenseRemoveMojo.java
+++ CopyrightAddingSoftware/Once edited Repository/LicenseRemoveMojo.java
@@ -1,19 +1,9 @@
-// =======================LicenseRemoveMojo.java=======================// 
-//   Author:    Connor Group Software Development team 2014            // 
-//   Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  // 
-//   Email:     it@connorgp.com                                        // 
-//   Website:   http://www.connorgp.com                                // 
-//                                                                     // 
-//   The copyright to the source code and computer program(s)          // 
-//   herein is the property of Connor Group.The source code            // 
-//   and program(s) may be used and/or copied only with the            // 
-//   written permission of Connor Group or in accordance with          // 
-//   the terms and conditions stipulated in the                        // 
-//   agreement/contract under which the source code and                // 
-//   program(s) have been supplied.                                    // 
-//                                                                     // 
-//=======================LicenseRemoveMojo.java=======================//
+
+
+
+
 package com.mycila.maven.plugin.license;
+
 import com.mycila.maven.plugin.license.document.Document;
 import com.mycila.maven.plugin.license.header.Header;
 import org.apache.maven.plugin.MojoExecutionException;
