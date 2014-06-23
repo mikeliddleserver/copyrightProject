4c20ba1d15f16a3ced30006bbcb98bbdfef9b2bb
mikeliddle <maliddle96@gmail.com>
2014-06-23 10:08:03 -0600

    added copyright headers

--- CopyrightAddingSoftware/repository/Form1.vb
+++ CopyrightAddingSoftware/repository/Form1.vb
@@ -1,3 +1,16 @@
+' ==============================Form1.vb==============================' 
+'   Author:    Connor Group Software Development team 2014            ' 
+'   Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  ' 
+'   Email:     it@connorgp.com                                        ' 
+'   Website:   http://www.connorgp.com                                ' 
+'                                                                     ' 
+'   The copyright to the source code and computer program(s)          ' 
+'   herein is the property of Connor Group.The source code            ' 
+'   and program(s) may be used and/or copied only with the            ' 
+'   written permission of Connor Group or in accordance with          ' 
+'   the terms and conditions stipulated in the                        ' 
+'   agreement/contract under which the source code and                ' 
+'   program(s) have been supplied.                                    ' 
+'                                                                     ' 
 Public Class Form1
-
 End Class
