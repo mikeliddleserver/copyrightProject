1d172c3f7c1e3fd27288c0330a54f08b82842608
Mike Liddle <mike.liddle@connorgp.com>
2014-06-25 17:06:45 -0600

    test

--- CopyrightAddingSoftware/repository/Default.java
+++ CopyrightAddingSoftware/repository/Default.java
@@ -1,9 +1,6 @@
 package com.mycila.maven.plugin.license;
 
-/**
- * @author Mathieu Carbou (mathieu.carbou@gmail.com)
- * @date 2013-08-01
- */
+
 public final class Default {
 
     public static final String[] INCLUDE = new String[]{"**"};
