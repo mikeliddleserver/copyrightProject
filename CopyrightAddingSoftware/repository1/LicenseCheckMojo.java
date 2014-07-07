ebcda4478904a8fbb5c69aa51216cac80803962f
Mike Liddle <mike.liddle@connorgp.com>
2014-07-07 13:22:08 -0600

    removed headers

--- CopyrightAddingSoftware/repository1/LicenseCheckMojo.java
+++ CopyrightAddingSoftware/repository1/LicenseCheckMojo.java
@@ -1,57 +1,6 @@
-//=======================LicenseCheckMojo.java========================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-<<<<<<< HEAD
-<<<<<<< HEAD
-//=======================LicenseCheckMojo.java========================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-<<<<<<< HEAD
-//=======================LicenseCheckMojo.java========================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-=======
 
->>>>>>> 0aeb2c99239193fb2bfbb634e8ff9013cd192ae5
-=======
-
->>>>>>> 38bdafccd2a821f2fa07d302a876f11a6922b02d
-=======
->>>>>>> 2664aa9f2caa2e67462ff988a8238b62c8d3217f
 package com.mycila.maven.plugin.license;
+
 import com.mycila.maven.plugin.license.document.Document;
 import com.mycila.maven.plugin.license.header.Header;
 import org.apache.maven.plugin.MojoExecutionException;
--- CopyrightAddingSoftware/repository2/LicenseFormatMojo.java
+++ CopyrightAddingSoftware/repository2/LicenseFormatMojo.java
@@ -1,60 +1,6 @@
-//=======================LicenseFormatMojo.java=======================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-<<<<<<< HEAD
-<<<<<<< HEAD
-//=======================LicenseFormatMojo.java=======================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-<<<<<<< HEAD
-//=======================LicenseFormatMojo.java=======================//
-//  Author:    Connor Group Software Development team 2014            //
-//  Copyright: Copyright (C) 2014 Connor Group. All rights reserved.  //
-//  Email:     it@connorgp.com                                        //
-//  Website:   http://www.connorgp.com                                //
-//                                                                    //
-//  The copyright to the source code and computer program(s)          //
-//  herein is the property of Connor Group.The source code            //
-//  and program(s) may be used and/or copied only with the            //
-//  written permission of Connor Group or in accordance with          //
-//  the terms and conditions stipulated in the                        //
-//  agreement/contract under which the source code and                //
-//  program(s) have been supplied.                                    //
-//                                                                    //
-=======
 
 
 
->>>>>>> 0aeb2c99239193fb2bfbb634e8ff9013cd192ae5
-=======
-
-
-
->>>>>>> 38bdafccd2a821f2fa07d302a876f11a6922b02d
-=======
->>>>>>> 2664aa9f2caa2e67462ff988a8238b62c8d3217f
 package com.mycila.maven.plugin.license;
 
 import com.mycila.maven.plugin.license.document.Document;
